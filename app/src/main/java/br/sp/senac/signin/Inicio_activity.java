bf1a7062d7150103beeba80f2258e8d25b20b955
Giovanni <giovannifossati11@gmail.com>
2020-04-02 18:57:12 -0300

    aaa

---
+++ app/src/main/java/br/sp/senac/signin/Inicio_activity.java
@@ -0,0 +1,14 @@
+package br.sp.senac.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inicio_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_layout);
+    }
+}
